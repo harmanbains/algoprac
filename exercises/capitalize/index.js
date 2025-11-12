1b1632d10f5fdfd2a292a1e6f3ddbb622c48023e
Harman Bains <bains.hmn@gmail.com>
2025-11-12 15:20:28 -0800

    capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let splitArr = str.split(' ');
+  let capArr = [];
+  for (let word of splitArr) {
+    let capWord = word.charAt(0).toUpperCase() + word.slice(1);
+    capArr.push(capWord);
+  }
+
+  return capArr.join(' ');
+}
 
 module.exports = capitalize;
